79981b3091c48bf3793e9c34dd9c819415bb46f4
abey.tom <abey.tom@appdynamics.com>
2017-12-07 11:46:35 -0500

    first commit

---
+++ src/main/java/com/atom/sample/atomsample/AtomSampleApplication.java
@@ -0,0 +1,12 @@
+package com.atom.sample.atomsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/atom/sample/atomsample/AtomSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atom.sample.atomsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtomSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
